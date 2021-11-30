5a6c8a8112d01f4202fe20490b25160bda671c97
RyanLucas18 <ryancampos@edu.unifil.br>
2021-11-30 17:03:27 -0300

    Recriando-Netflix

---
+++ js/owl/setup.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        400:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        },
+        1500:{
+            items:6
+        },
+        2000:{
+            items:8
+        }
+    }
+})
